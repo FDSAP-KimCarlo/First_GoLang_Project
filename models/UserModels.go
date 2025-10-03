7d61051cfb5e2944916a3b0aafd86a22dfdf160a
kimcarlomendoza-hue <kimcarlo.mendoza@fortress-asya.com>
2025-10-03 15:54:10 +0800

    Altered the model to have dates of creation, update, and delete

--- models/UserModels.go
+++ models/UserModels.go
@@ -1,15 +1,24 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Customer struct {
-	ID          int    `gorm:"column:customerid;primaryKey;autoIncrement" json:"id"`
-	Name        string `gorm:"column:customername" json:"name"`
-	ContactName string `gorm:"column:contactname" json:"contactName"`
-	Address     string `gorm:"column:address" json:"address"`
-	City        string `gorm:"column:city" json:"city"`
-	PostalCode  string `gorm:"column:postalcode" json:"postalCode"`
-	Country     string `gorm:"column:country" json:"country"`
+	ID          int            `gorm:"column:customerid;primaryKey;autoIncrement" json:"id"`
+	Name        string         `gorm:"column:customername" json:"name"`
+	ContactName string         `gorm:"column:contactname" json:"contactName"`
+	Address     string         `gorm:"column:address" json:"address"`
+	City        string         `gorm:"column:city" json:"city"`
+	PostalCode  string         `gorm:"column:postalcode" json:"postalCode"`
+	Country     string         `gorm:"column:country" json:"country"`
+	CreatedAt   time.Time      `gorm:"column:created_at" json:"createdAt"`
+	UpdatedAt   time.Time      `gorm:"column:updated_at" json:"updatedAt"`
+	DeletedAt   gorm.DeletedAt `gorm:"column:deleted_at;index" json:"deletedAt,omitempty"`
 }
 
-func (Customer) TableName() string {
+func (Customer) TableCustomer() string {
 	return "customers"
 }
